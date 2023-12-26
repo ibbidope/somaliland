6b9665f42f831977fe7974fcaf4784e8c0cd09c2
ibbidope <ibbidope@gmail.com>
2023-12-26 12:44:55 +0500

    modified:   App.tsx

--- App.tsx
+++ App.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
 import AppNavigator from './src/navigation/AppNavigator';
+import {SafeAreaView} from 'react-native';
 
 const App = () => {
-  return <AppNavigator />;
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <AppNavigator />
+    </SafeAreaView>
+  );
 };
 
 export default App;
